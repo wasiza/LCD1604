4e1bb825a88fe54082f6d6fc509b2738a4c79a76
wasiza <36593701+wasiza@users.noreply.github.com>
2023-09-01 23:33:44 +0300

    lcd_2Line.py

--- lcd_2.py
+++ lcd_2.py
@@ -1,28 +1,26 @@
 from RPLCD.i2c import CharLCD
-import time
+import sys
 
-# Set up the LCD
-lcd = CharLCD('PCF8574', 0x27, cols=16, rows=4)
+# Initialize the LCD
+lcd = CharLCD('PCF8574', 0x27, cols=16, rows=2)
 
-# Clear the LCD display
-lcd.clear()
-
-def update_lcd(line1, line2, line3, line4):
-    lcd.cursor_pos = (0, 0)
-    lcd.write_string(line1)
-    lcd.cursor_pos = (1, 0)
-    lcd.write_string(line2)
-    lcd.cursor_pos = (2, 0)
-    lcd.write_string(line3)
-    lcd.cursor_pos = (3, 0)
-    lcd.write_string(line4)
-
-# Display a message over 4 lines
-update_lcd("This is the", "GPT-3.5 Assistant,", "here to help you", "Have a great day")
-
-# Keep the message displayed
 try:
-    while True:
-        time.sleep(1)  # Keep the LCD message displayed
-except KeyboardInterrupt:
-    lcd.clear()  # Clear the LCD display
+    # Display "Hello" on the first line
+    lcd.write_string("Hello")
+    
+    # Set the cursor to the beginning of the second line
+    lcd.cursor_pos = (1, 0)
+    
+    # Display "World!" on the second line
+    lcd.write_string("World!")
+
+    # Print "Hello, World!" on the terminal
+    print("Hello, World!")
+
+    # Wait for user input to exit
+    input("Press Enter to exit...")
+
+finally:
+    # Clear the display and close the LCD
+    lcd.clear()
+    lcd.close()
