d86db48f7dcddc60bd16110a20a3c516518d63d7
wasiza <36593701+wasiza@users.noreply.github.com>
2023-09-01 23:39:07 +0300

    Lcd_4line.py

--- lcd.py
+++ lcd.py
@@ -5,25 +5,25 @@ lcd = CharLCD('PCF8574', 0x27, cols=20, rows=4)  # Change 'cols' to 20 and 'rows
 
 try:
     # Display "Hello" on the first line
-    lcd.write_string("Hello")
+    lcd.write_string("Coding is")
     
     # Set the cursor to the beginning of the second line
     lcd.cursor_pos = (1, 0)
     
     # Display "World!" on the second line
-    lcd.write_string("World!")
+    lcd.write_string("an art")
     
     # Set the cursor to the beginning of the third line
     lcd.cursor_pos = (2, 0)
     
     # Display "GPT!" on the third line
-    lcd.write_string("GPT!")
+    lcd.write_string("Let's create")
     
     # Set the cursor to the beginning of the fourth line
     lcd.cursor_pos = (3, 0)
     
     # Display "3.5!" on the fourth line
-    lcd.write_string("3.5!")
+    lcd.write_string("something amazing!")
 
     # Wait for user input to exit
     input("Press Enter to exit...")
