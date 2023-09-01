8a0b3aa87f05317a2bf9826e06910d0070aef92c
wasiza <36593701+wasiza@users.noreply.github.com>
2023-09-01 23:36:21 +0300

    lcd_4line.py

--- lcd.py
+++ lcd.py
@@ -1,47 +1,34 @@
 from RPLCD.i2c import CharLCD
-import time
-import sys
 
-# Set up the LCD
-lcd = CharLCD('PCF8574', 0x27, cols=16, rows=4)
+# Initialize the LCD
+lcd = CharLCD('PCF8574', 0x27, cols=20, rows=4)  # Change 'cols' to 20 and 'rows' to 4 for a 4-line LCD
 
-# Clear the LCD display
-lcd.clear()
-
-# Display a message on each of the four lines
-lines = [
-    "Hello, this is a",
-    "Custom message on",
-    "Your LCD display!",
-    "Have a great day!"
-]
-
-# Truncate or pad messages to fit within 16 characters
-for i in range(4):
-    lines[i] = lines[i][:16].ljust(16)
-
-# Loop through each line and display the message
-for i in range(4):
-    lcd.cursor_pos = (i, 0)  # Set cursor position to the current line
-    lcd.write_string(lines[i])
-
-def exit_program():
-    lcd.clear()  # Clear the LCD display
-    lcd.write_string("Exiting...")  # Display "Exiting" message
-
-    # Fade in and turn on the backlight
-    for brightness in range(0, 101, 10):
-        lcd.backlight_enabled = True
-        lcd.backlight_pwm = brightness
-        time.sleep(0.2)  # Delay for fade-in effect
-
-    lcd.clear()  # Clear the LCD display
-    lcd.backlight_enabled = False  # Turn off the backlight
-    sys.exit()  # Terminate the program
-
-# Keep the message displayed and handle KeyboardInterrupt
 try:
-    while True:
-        pass
-except KeyboardInterrupt:
-    exit_program()
+    # Display "Hello" on the first line
+    lcd.write_string("Hello")
+    
+    # Set the cursor to the beginning of the second line
+    lcd.cursor_pos = (1, 0)
+    
+    # Display "World!" on the second line
+    lcd.write_string("World!")
+    
+    # Set the cursor to the beginning of the third line
+    lcd.cursor_pos = (2, 0)
+    
+    # Display "GPT!" on the third line
+    lcd.write_string("GPT!")
+    
+    # Set the cursor to the beginning of the fourth line
+    lcd.cursor_pos = (3, 0)
+    
+    # Display "3.5!" on the fourth line
+    lcd.write_string("3.5!")
+
+    # Wait for user input to exit
+    input("Press Enter to exit...")
+
+finally:
+    # Clear the display and close the LCD
+    lcd.clear()
+    lcd.close()
