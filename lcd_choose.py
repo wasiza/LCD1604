d65f466d232c32995d15e88c9730c5e7b8bb8a38
wasiza <36593701+wasiza@users.noreply.github.com>
2023-09-02 04:41:56 +0300

    lcd_choose.py

--- lcd_choose.py
+++ lcd_choose.py
@@ -7,9 +7,9 @@ lcd = CharLCD('PCF8574', 0x27, cols=16, rows=2, backlight_enabled=True)
 try:
     while True:
         lcd.clear()
-        lcd.write_string("Go the beach? or")
+        lcd.write_string("Go to the beach?")
         lcd.cursor_pos = (1, 0)
-        lcd.write_string("Stay at home?")
+        lcd.write_string("Or stay at home?")
         
         user_input = input("Select an option (1/2): ")
         
